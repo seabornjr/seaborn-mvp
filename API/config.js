70e36a74d76da3be9dc89aba221387d5bf1fa8a8
Shelton.Seaborn <seabornranger@aol.com>
2022-10-12 15:31:05 -0700

    trying Dylans config code

--- API/config.js
+++ API/config.js
@@ -1,14 +1,12 @@
 
 
 module.exports = {
-    
-    dev: {
-        connectionString: 'postgres://postgres:postgrespw@localhost:55000/artistlist',
-        PORT: 8002
-    },
-    
-    production: {
-        connectionString: process.env.POSTGRES_CONNECTION_STRING + "?ssl=true",
-        port: process.env.PORT
-    }
+  dev: {
+    connectionString:'postgres://postgres:postgrespw@localhost:55000/artistlist',
+    port: '8002'
+  },
+  production: {
+    connectionString: process.env.POSTGRES_CONNECTION_STRING + "?ssl=true",
+    port: process.env.PORT
+  }
 }
